cc23a7667d95c1669b923a9b825931297d86a636
ToeCrow <thomas.kronvold@chasacademy.se>
2025-04-10 13:37:56 +0200

    easy breezy

--- server.js
+++ server.js
@@ -12,14 +12,35 @@ app.get('/', (req, res) => {
   res.send('Welcome to the jungle')
 })
 
+// 1
 app.get('/api/welcome', (req, res) => {
   res.header('Custom-Message', 'Välkommen hit!');
   res.send('Checka dina headers! 👀')
 });
 
+// 2
 app.get('/api/headers', (req, res) => {
   res.json(req.headers);
 });
+
+// 3
+app.get('/api/name', (req, res) => {
+  res.json({ name: 'Thomas'});
+})
+
+// 4
+app.get('/api/greet/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ message: `Hej ${name}!`});
+});
+
+// 5
+app.get('/api/greet', (req, res) => {
+  const name = req.query.name;
+  res.json({ message: `Hej ${name}!`});
+});
+
+// 6
 
 
 app.listen(PORT, () => {
