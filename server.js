88c795a5e03f59be352bc11e4c1233e50a9dce67
ToeCrow <thomas.kronvold@chasacademy.se>
2025-04-10 12:50:01 +0200

    one and done

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the jungle')
+})
+
+app.get('/api/welcome', (req, res) => {
+  res.header('Custom-Message', 'Välkommen hit!');
+  res.send('Checka dina headers! 👀')
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
